147505283d4e2db197ed77b00d9fff9eb916a932
npratik0 <neupanepratik00@gmail.com>
2026-01-02 16:05:53 +0545

    Hive Table constant added

---
+++ lib/core/constants/hive_table_constant.dart
@@ -0,0 +1,8 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  static const String dbName = 'frendly_db';
+
+  static const int authTypeId = 0;
+  static const String authTable = 'auth_table';
+}
