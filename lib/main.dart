bd1054f02d086a1f5ed0b03b13d73ac4b68e89f2
npratik0 <neupanepratik00@gmail.com>
2026-01-02 16:25:18 +0545

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frendly/core/constants/hive_table_constant.dart';
+import 'package:frendly/features/auth/data/models/auth_hive_models.dart';
 import 'package:frendly/screens/bottom_screen/home_screen.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 import 'screens/splash_screen.dart';
 import 'screens/onboarding_screen.dart';
-import 'screens/login_screen.dart';
-import 'screens/register_screen.dart';
+import 'features/auth/presentation/pages/login_screen.dart';
+import 'features/auth/presentation/pages/register_screen.dart';
 import 'screens/bottom_navigation_screen.dart';
 
-void main() {
-  runApp(const FrendlyApp());
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+
+  // await Hive.initFlutter();
+  // Hive.registerAdapter(AuthHiveModelsAdapter());
+  // await Hive.openBox<AuthHiveModels>('usersBox');
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// ✅ HIVE INITIALIZATION (CRITICAL)
+  final directory = await getApplicationDocumentsDirectory();
+  final path = '${directory.path}/${HiveTableConstant.dbName}';
+  Hive.init(path);
+
+  if (!Hive.isAdapterRegistered(HiveTableConstant.authTypeId)) {
+    Hive.registerAdapter(AuthHiveModelsAdapter());
+  }
+
+  await Hive.openBox<AuthHiveModels>(HiveTableConstant.authTable);
+
+  // runApp(const FrendlyApp());
+  runApp(const ProviderScope(child: FrendlyApp()));
 }
 
 class FrendlyApp extends StatelessWidget {
@@ -31,3 +57,23 @@ class FrendlyApp extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:hive_flutter/hive_flutter.dart';
+// import 'package:frendly/features/auth/data/models/auth_hive_models.dart';
+// import 'app.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   await Hive.initFlutter();
+//   Hive.registerAdapter(AuthHiveModelsAdapter());
+//   await Hive.openBox<AuthHiveModels>('usersBox');
+
+//   runApp(
+//     const ProviderScope(
+//       child: FrendlyApp(),
+//     ),
+//   );
+// }
